51fb49e4cdfb22ea10ce4f7738e063328a2b70f3
zhumingxiao <tlimit@outlook.com>
2024-10-01 08:50:42 +0800

    fix init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local vim = vim or {}     -- 防止 lsp 报错找不到 vim
 local set = vim.o
-#set.clipboard = "unnamed" -- 复制到剪切板 待安装额外插件
+--set.clipboard = "unnamed" -- 复制到剪切板 待安装额外插件
 set.number = true         -- 显示行号
 set.relativenumber = true -- 显示相对行号
 set.cursorline = true     -- 高亮光标所在行
